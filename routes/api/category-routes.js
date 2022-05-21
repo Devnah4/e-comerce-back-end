a3477e6b990ffc1b13266e411f008eedb19ec2ed
Martin C <98830462+Devnah4@users.noreply.github.com>
2022-05-21 18:53:09 -0400

    can pull categories

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -7,14 +7,18 @@ router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
   Category.findAll({
-    attributes: ['id', 'category_name'],
     include: [
       {
         model: Product,
-        attributes: ['id', 'category_name', 'price', 'stock', 'category_id'],
-      },
-    ],
+        attributes: ['id', 'product_name', 'price', 'stock', 'category_id'],
+      }
+    ]
   })
+    .then(dbCategoryData => res.json(dbCategoryData))
+    .catch(err => {
+      console.log(err);
+      res.status(500).json(err);
+    });
 });
 
 router.get('/:id', (req, res) => {
